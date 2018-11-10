2f63b4aa772e91f5e1463a804390257dee2d55fa
Sean Juhwan Son <hwannee@gmail.com>
2018-11-10 16:25:07 -0500

    Initial commit

---
+++ ConsoleApp1/JsonFeed.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class JsonFeed
+    {
+        static string _url = "";
+        static int _results = 42; // joke_id
+        
+        public JsonFeed() { }
+        public JsonFeed(string endpoint, int results)
+        {
+            _url = endpoint;
+            _results = results; 
+        }
+        
+		public static string[] GetRandomJokes(string firstname, string lastname, string category)
+		{
+			HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri(_url);
+			string url = "jokes/";
+            string[] jokes = new string[_results];
+
+            for(int i =  0; i < _results; i++)
+            {
+                int id = (new Random()).Next(1, 603);
+                //TODO: handle id exception
+                url += id.ToString();
+                if (firstname != null)
+                {
+                    if (url.Contains('?'))
+                        url += "&";
+                    else url += "?";
+                    url += "firstName=";
+                    url += firstname.ToString();
+                }
+                if (lastname != null)
+                {
+                    if (url.Contains('?'))
+                        url += "&";
+                    else url += "?";
+                    url += "lastName=";
+                    url += lastname.ToString();
+                }
+                if (category != null)
+                {
+                    if (url.Contains('?'))
+                        url += "&";
+                    else url += "?";
+                    url += "limitTo=[";
+                    url += category;
+                    url += "]";
+                }
+                jokes[i] = Task.FromResult(client.GetStringAsync(url).Result).Result;
+            }            
+
+			return jokes;
+		}
+
+        /// <summary>
+        /// returns an object that contains name and surname
+        /// </summary>
+        /// <param name="client2"></param>
+        /// <returns></returns>
+		public static dynamic GetName()
+		{
+			HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri(_url);
+			var result = client.GetStringAsync("").Result;
+			return JsonConvert.DeserializeObject<dynamic>(result);
+		}
+
+		public static string[] GetCategories()
+		{
+			HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri(_url);
+
+			return new string[] { Task.FromResult(result: client.GetStringAsync("categories").Result).Result };
+		}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string[] results = new string[50];
+        static char key;
+        static Tuple<string, string> name;
+        static ConsolePrinter printer = new ConsolePrinter();
+
+        static void Main(string[] args)
+        {
+            printer.Value("Press h to get instructions.").ToString();
+            GetEnteredKey(Console.ReadKey());
+
+            if(key == 'h')
+            {
+                while (true)
+                {
+                    printer.Value(Environment.NewLine + "Press c to get categories").ToString();
+                    printer.Value("Press r to get random jokes").ToString();
+                    GetEnteredKey(Console.ReadKey());
+                    if (key == 'c')
+                    {
+                        GetCategories();
+                        PrintResults();
+                    }
+                    if (key == 'r')
+                    {
+                        printer.Value(Environment.NewLine + "Want to use a random name? y/n").ToString();
+                        GetEnteredKey(Console.ReadKey());
+                        if (key == 'y')
+                        {
+                            GetNames();
+                            printer.Value(Environment.NewLine + name).ToString();
+                        }
+                        else
+                        {
+                            // TODO: custom name
+                        }
+                            
+                        printer.Value(Environment.NewLine + "Want to specify a category? y/n").ToString();
+                        GetEnteredKey(Console.ReadKey());
+                        if (key == 'y')
+                        {
+                            printer.Value(Environment.NewLine + "How many jokes do you want? (1-9)").ToString();
+                            int n = Int32.Parse(Console.ReadLine());
+                            printer.Value("Enter a category;").ToString();
+                            GetRandomJokes(Console.ReadLine(), n);
+                            PrintResults();
+                        }
+                        else 
+                        {
+                            printer.Value(Environment.NewLine + "How many jokes do you want? (1-9)").ToString();
+                            int n = Int32.Parse(Console.ReadLine());
+                            GetRandomJokes(null, n);
+                            PrintResults();
+                        }
+                    }
+                    name = null;
+                }
+            }
+
+        }
+
+        private static void PrintResults()
+        {
+            printer.Value("[" + string.Join(",", results) + "]").ToString();
+        }
+
+        private static void GetEnteredKey(ConsoleKeyInfo consoleKeyInfo)
+        {
+            switch (consoleKeyInfo.Key)
+            {
+                case ConsoleKey.C:
+                    key = 'c';
+                    break;
+                case ConsoleKey.D0:
+                    key = '0';
+                    break;
+                case ConsoleKey.D1:
+                    key = '1';
+                    break;
+                case ConsoleKey.D3:
+                    key = '3';
+                    break;
+                case ConsoleKey.D4:
+                    key = '4';
+                    break;
+                case ConsoleKey.D5:
+                    key = '5';
+                    break;
+                case ConsoleKey.D6:
+                    key = '6';
+                    break;
+                case ConsoleKey.D7:
+                    key = '7';
+                    break;
+                case ConsoleKey.D8:
+                    key = '8';
+                    break;
+                case ConsoleKey.D9:
+                    key = '9';
+                    break;
+                case ConsoleKey.R:
+                    key = 'r';
+                    break;
+                case ConsoleKey.Y:
+                    key = 'y';
+                    break;
+                case ConsoleKey.N:
+                    key = 'n';
+                    break;
+                case ConsoleKey.H:
+                    key = 'h';
+                    break;
+            }
+        }
+
+        private static void GetRandomJokes(string category, int number)
+        {            
+            new JsonFeed("http://api.icndb.com", number);
+            results = JsonFeed.GetRandomJokes(name?.Item1, name?.Item2, category);
+        }
+
+        private static void GetCategories()
+        {
+            new JsonFeed("http://api.icndb.com", 0);
+            results = JsonFeed.GetCategories();
+        }
+
+        private static void GetNames()
+        {
+            new JsonFeed("http://uinames.com/api", 0);
+            dynamic result = JsonFeed.GetName();
+            name = Tuple.Create(result.name.ToString(), result.surname.ToString());
+        }
+    }
+}
